2d42c14f3915429b53cc14c5a28bd09a07770acb
Andre Rodrigues <lrandre601@gmail.com>
2022-03-20 23:57:39 -0300

    finalizando identify e correção de bugs

--- lexer.java
+++ lexer.java
@@ -2,9 +2,9 @@ public class lexer{
     public static String lexema_atual = "";
     public static int token_id = 0;
     public static String numero = "";
+    public static String s = "";
     public static int index = 0;
     public static String word = "a := (aux - 2) * 200 / 19";
-
 
     private static void identifyTokenPairs(char character, char nextCharacter){
         if(character == '=' && nextCharacter == '='){
@@ -26,12 +26,21 @@ public class lexer{
             lexema_atual = "";
         }
         else if(character >= 'a' && character <= 'z' || character >= 'A' && character <= 'Z'){
-             /**
-             * ARRUMAR:
-             * Está retornando o lexema de cada letra da palavra
-             * Se for num com mais de 1 letra, deve retornar um lexema apenas, e não vários
-             */
-            tokenIdent(character);
+            while(index < word.length()){
+                if((word.charAt(index) >= 'a' && word.charAt(index) <= 'z') || (word.charAt(index) >= 'A' && word.charAt(index) <= 'Z')){
+                    s += String.valueOf(word.charAt(index));
+                    index++;
+                }else{
+                    tokenIdent(s);
+                    index--;
+                    s = "";
+                    break;
+                }
+            }
+
+            if(index == word.length()){
+                tokenIdent(s);
+            }
         }
         else if(character >= '0' && character <= '9'){
             while(index < word.length()){
@@ -62,10 +71,10 @@ public class lexer{
         }
     }
 
-    private static void tokenIdent(char character){
+    private static void tokenIdent(String s){
         token_id = 1;
         lexema_atual = "IDENT";
-        System.out.printf("( %s, %s, %s )\n", character, lexema_atual, token_id);
+        System.out.printf("( %s, %s, %s )\n", s, lexema_atual, token_id);
     }
 
     private static void tokenInteger(String num){
@@ -145,6 +154,22 @@ public class lexer{
         return '\0';
     } 
     public static void main(String[] args) {
+        /**
+         * FAZER
+         * Considere que os lexemas relativos aos números terão tamanho menor 100 dígitos. A mesma restrição de tamanho se aplica aos nomes de variáveis.
+         */
+        word = word.trim();
+        System.out.println(word);
+        if(word.charAt(0) >= '0' && word.charAt(0) <= '9' ){
+            System.out.println("Entrada invalida...");
+            System.exit(0);
+        }
+
+        if(word.contains(",") || word.contains(".") || word.contains("-0") || word.contains("-1") || word.contains("-2") || word.contains("-3") || word.contains("-4") || word.contains("-5") || word.contains("-6") || word.contains("-7") || word.contains("-8") || word.contains("-9")){
+            System.out.println("Entrada invalida...");
+            System.exit(0);
+        }
+
         System.out.println(readCharacter(word));    
     }
 }
